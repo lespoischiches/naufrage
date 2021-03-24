c5f20aa5752882d85ab322a55174b6e9f1880036
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-03-24 17:55:04 +0100

    Implement events list

--- app/src/main/java/com/thomas/bateau/EvenementAccueilFragment.java
+++ app/src/main/java/com/thomas/bateau/EvenementAccueilFragment.java
@@ -1,15 +1,25 @@
 package com.thomas.bateau;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class EvenementAccueilFragment extends Fragment {
+
+    private View fragView;
+
     public EvenementAccueilFragment() {
         //
     }
@@ -17,6 +27,10 @@ public class EvenementAccueilFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.evenement_accueil_fragment, container, false);
+        fragView=inflater.inflate(R.layout.evenement_accueil_fragment, container, false);
+        ListView listView=fragView.findViewById(R.id.frag_evenements_list);
+        List<String> listItems=Arrays.asList("Hello", "World");
+        listView.setAdapter(new ArrayAdapter<String>(fragView.getContext(), android.R.layout.simple_list_item_1, listItems));
+        return fragView;
     }
 }
