bf2f1d433ef7556b0786c385030d5c58369c2f9d
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-03-31 15:48:48 +0200

    Add notification on click on listView

---
+++ app/src/main/java/com/thomas/bateau/BateauApplication.java
@@ -0,0 +1,37 @@
+package com.thomas.bateau;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import java.util.Objects;
+
+public class BateauApplication extends Application {
+
+    public static final String CHANNEL_1_ID="channel low";
+    public static final String CHANNEL_2_ID="channel default";
+    public static final String CHANNEL_3_ID="channel high";
+    private static NotificationManager notificationManager;
+
+    private void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1=new NotificationChannel(CHANNEL_1_ID, "channel 1", NotificationManager.IMPORTANCE_LOW);
+            NotificationChannel channel2=new NotificationChannel(CHANNEL_2_ID, "channel 2", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel3=new NotificationChannel(CHANNEL_3_ID, "channel 3", NotificationManager.IMPORTANCE_HIGH);
+            channel1.setDescription("Low priority");
+            channel2.setDescription("Default priority");
+            channel3.setDescription("High priority");
+            notificationManager=getSystemService(NotificationManager.class);
+            Objects.requireNonNull(notificationManager).createNotificationChannel(channel1);
+            Objects.requireNonNull(notificationManager).createNotificationChannel(channel2);
+            Objects.requireNonNull(notificationManager).createNotificationChannel(channel3);
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+}
--- app/src/main/java/com/thomas/bateau/EvenementAccueilFragment.java
+++ app/src/main/java/com/thomas/bateau/EvenementAccueilFragment.java
@@ -2,23 +2,30 @@ package com.thomas.bateau;
 
 import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static com.thomas.bateau.BateauApplication.CHANNEL_1_ID;
+
 public class EvenementAccueilFragment extends Fragment {
 
     private View fragView;
+    private int notificationId=0;
 
     public EvenementAccueilFragment() {
         //
@@ -31,6 +38,17 @@ public class EvenementAccueilFragment extends Fragment {
         ListView listView=fragView.findViewById(R.id.frag_evenements_list);
         List<String> listItems=Arrays.asList("Hello", "World");
         listView.setAdapter(new ArrayAdapter<String>(fragView.getContext(), android.R.layout.simple_list_item_1, listItems));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                sendNotificationOnChannel("NOTIF !!", "Hello from notification", CHANNEL_1_ID, NotificationCompat.PRIORITY_DEFAULT);
+            }
+        });
         return fragView;
+    }
+
+    private void sendNotificationOnChannel(String title, String message, String channelId, int priority) {
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(getActivity().getApplicationContext(), channelId).setContentTitle(title).setSmallIcon(R.drawable.cloud_icon).setContentText(message).setPriority(priority);
+        NotificationManagerCompat.from(getActivity()).notify(++notificationId, notification.build());
     }
 }
