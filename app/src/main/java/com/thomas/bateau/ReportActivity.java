d962f5c709ebd67eabd58a6852c1cfaaa28113af
Arthur-Soens <71391727+Arthur-Soens@users.noreply.github.com>
2021-03-31 17:11:21 +0200

    petit bouton

--- app/src/main/java/com/thomas/bateau/ReportActivity.java
+++ app/src/main/java/com/thomas/bateau/ReportActivity.java
@@ -2,13 +2,21 @@ package com.thomas.bateau;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ReportActivity extends AppCompatActivity {
+
+    private Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_activity);
+        btnBack = findViewById(R.id.btn_retour_signalement);
+        btnBack.setOnClickListener(click ->{
+            finish();
+        });
     }
 }
