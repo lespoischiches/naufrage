b9d185680d02e71d1db70f88c56fca90724c84b0
Arthur-Soens <71391727+Arthur-Soens@users.noreply.github.com>
2021-03-31 16:59:13 +0200

    petit commit

--- app/src/main/java/com/thomas/bateau/MainActivity.java
+++ app/src/main/java/com/thomas/bateau/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnMeteo, btnCarte;
+    private Button btnMeteo, btnCarte, btnReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +16,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnMeteo=findViewById(R.id.btn_accueil_meteo);
         btnCarte=findViewById(R.id.btn_accueil_carte);
+        btnReport=findViewById(R.id.btn_accueil_signaler_evenement);
         btnMeteo.setOnClickListener(click -> {
             Intent intent=new Intent(getApplicationContext(), MeteoActivity.class);
             startActivity(intent);
         });
         btnCarte.setOnClickListener(click -> {
             Intent intent=new Intent(getApplicationContext(), CarteActivity.class);
+            startActivity(intent);
+        });
+
+        btnReport.setOnClickListener(click -> {
+            Intent intent = new Intent(getApplicationContext(), ReportActivity.class);
             startActivity(intent);
         });
     }
