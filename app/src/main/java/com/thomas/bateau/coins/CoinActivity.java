a1df51aa3ac1927a72ea117e37d8c320381e0f51
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-04-07 16:30:44 +0200

    Coin -> add retour button

--- app/src/main/java/com/thomas/bateau/coins/CoinActivity.java
+++ app/src/main/java/com/thomas/bateau/coins/CoinActivity.java
@@ -15,8 +15,11 @@ public class CoinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coin);
-        coinsFactory=new CoinPecheurFactory();
+        coinsFactory=new CoinPecheurFactory(); // Si pecheur, sinon CoinScientifiqueFactory, CoinSkipperFactory etc...
         getFragmentManager().beginTransaction().replace(R.id.coin_result_fragment, (android.app.Fragment)coinsFactory.createFragmentResults()).addToBackStack(null).commit();
         getFragmentManager().beginTransaction().replace(R.id.coin_search_fragment, (android.app.Fragment)coinsFactory.createFragmentSearch()).addToBackStack(null).commit();
+        findViewById(R.id.coin_retour_bouton).setOnClickListener(c -> {
+            finish();
+        });
     }
 }
