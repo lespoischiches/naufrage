b2bd4c00acfb6cc647dbd9f211a181fcd808b65e
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-04-08 19:52:20 +0200

    Location - abstract class (must be improved)

--- app/src/main/java/com/thomas/bateau/CarteActivity.java
+++ app/src/main/java/com/thomas/bateau/CarteActivity.java
@@ -13,17 +13,15 @@ import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
 import org.osmdroid.views.overlay.OverlayItem;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 
 import java.util.ArrayList;
 
-public class CarteActivity extends AppCompatActivity {
+public class CarteActivity extends LocationAccessActivity {
 
     private MapView map;
     private Button btnRetour;
     private IMapController mapController;
-    private LocationAccess locationAccess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +29,35 @@ public class CarteActivity extends AppCompatActivity {
         Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         setContentView(R.layout.activity_carte);
         btnRetour=findViewById(R.id.carte_btnretour);
-        locationAccess=new LocationAccess(this);
         btnRetour.setOnClickListener(click -> {
             finish();
         });
         map=findViewById(R.id.carte_osm);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
-        double[] location=locationAccess.getLocation();
-        if(location == null) {
-            location=new double[]{39.5711111, 126.05555555555556};
-        }
-        GeoPoint startPoint=new GeoPoint(location[0], location[1]);
-        mapController=map.getController();
-        mapController.setCenter(startPoint);
-        mapController.setZoom(18d);
 
+
+        mapController=map.getController();
+        mapController.setZoom(18d);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    void addLocationOverlayToMap(Double[] location) {
+        GeoPoint startPoint=new GeoPoint(location[0], location[1]);
+        mapController.setCenter(startPoint);
         ArrayList<OverlayItem> items=new ArrayList<>();
-        items.add(new OverlayItem("Your location", "You're here !", new GeoPoint(location[0], location[1])));
+        items.add(new OverlayItem("Your location", "You're here !", startPoint));
 
         ItemizedOverlayWithFocus<OverlayItem> mOverlay=new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(), items, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
             @Override
@@ -64,31 +73,14 @@ public class CarteActivity extends AppCompatActivity {
 
         mOverlay.setFocusItemsOnTap(true);
         map.getOverlays().add(mOverlay);
-
-
-        //Log.d("A", String.valueOf(locationAccess.getLocation()[0]));
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        map.onPause();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        map.onResume();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        locationAccess.onDestroy();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        locationAccess.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public void onNewLocationAvailable() {
+        Double[] location=getLocation();
+        if(location == null) {
+            location=new Double[]{39.5711111, 126.05555555555556};
+        }
+        addLocationOverlayToMap(location);
     }
 }
--- app/src/main/java/com/thomas/bateau/LocationAccessActivity.java
+++ app/src/main/java/com/thomas/bateau/LocationAccessActivity.java
@@ -6,14 +6,16 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 import static android.Manifest.permission.*;
 
-public class LocationAccess {
+public abstract class LocationAccessActivity extends AppCompatActivity {
 
-    private Context context;
     private ArrayList<String> permissionsToRequest;
     private ArrayList<String> permissionsRejected = new ArrayList();
     private ArrayList permissions = new ArrayList();
@@ -21,26 +23,32 @@ public class LocationAccess {
     private final static int ALL_PERMISSIONS_RESULT = 101;
     MyLocationService myLocationService;
 
-    public LocationAccess(Context c) {
-        context=c;
+    public LocationAccessActivity() {
         permissions.add(ACCESS_FINE_LOCATION);
         permissions.add(ACCESS_COARSE_LOCATION);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        myLocationService=new MyLocationService(this);
         permissionsToRequest = findUnAskedPermissions(permissions);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
 
             if (permissionsToRequest.size() > 0) {
-                ((Activity)context).requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]), ALL_PERMISSIONS_RESULT);
+                requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]), ALL_PERMISSIONS_RESULT);
+            } else {
+                onNewLocationAvailable();
             }
         }
-        myLocationService=new MyLocationService(context);
     }
 
-    public double[] getLocation() {
+    public Double[] getLocation() {
         if (!myLocationService.canGetLocation()) {
             return null;
         }
-        double[] result={myLocationService.getLatitude(), myLocationService.getLongitude()};
+        Double[] result={myLocationService.getLatitude(), myLocationService.getLongitude()};
         return result;
     }
 
@@ -58,7 +66,7 @@ public class LocationAccess {
     private boolean hasPermission(String permission) {
         if (canMakeSmores()) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                return (context.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
+                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
             }
         }
         return true;
@@ -68,10 +76,13 @@ public class LocationAccess {
         return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
     }
 
-    void onDestroy() {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
         myLocationService.stopListener();
     }
 
+    @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case ALL_PERMISSIONS_RESULT:
@@ -82,23 +93,30 @@ public class LocationAccess {
                 }
                 if (permissionsRejected.size() > 0) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        if (((Activity)context).shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                        if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
                             showMessageOKCancel("These permissions are mandatory for the application. Please allow access.", new DialogInterface.OnClickListener() {
                                         @Override
                                         public void onClick(DialogInterface dialog, int which) {
                                             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                                ((Activity)context).requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
+                                                requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
                                             }
                                         }
                                     });
                             return;
                         }
                     }
+                } else {
+                    onNewLocationAvailable();
                 }
                 break;
         }
     }
+
     private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(context).setMessage(message).setPositiveButton("OK", okListener).setNegativeButton("Cancel", null).create().show();
+        new AlertDialog.Builder(this).setMessage(message).setPositiveButton("OK", okListener).setNegativeButton("Cancel", null).create().show();
     }
+
+    public void onNewLocationAvailable() {}
+
+
 }
