099bfa1846c4e12de805bcd968a9948e8e7c7120
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-04-14 16:01:23 +0200

    Set MVC to MeteoActivity

--- app/src/main/java/com/thomas/bateau/meteo/MeteoActivity.java
+++ app/src/main/java/com/thomas/bateau/meteo/MeteoActivity.java
@@ -4,20 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.thomas.bateau.R;
 
-public class MeteoActivity extends AppCompatActivity {
+import java.util.Observable;
+import java.util.Observer;
 
-    private Button btnRetour;
+public class MeteoActivity extends AppCompatActivity implements Observer {
+
+    private Button btnRetour, btnSwitchModeShoreOffShore;
+    private TextView textTemp;
+    private MeteoModel meteoModel=new MeteoModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meteo);
+        meteoModel.addObserver(this);
         btnRetour=findViewById(R.id.btn_retour_meteo);
+        textTemp=findViewById(R.id.meteo_text_temp);
+        btnSwitchModeShoreOffShore=findViewById(R.id.meteo_btn_switch_mode_shore_offshore);
         btnRetour.setOnClickListener(click -> {
             finish();
         });
+        btnSwitchModeShoreOffShore.setOnClickListener(click -> {
+            meteoModel.setMeteoType(MeteoModel.MeteoType.listTypes[(meteoModel.getMeteoType().getIndex()+1) % MeteoModel.MeteoType.listTypes.length]);
+        });
+        btnSwitchModeShoreOffShore.setText(meteoModel.getMeteoType().toString());
+        textTemp.setText(String.valueOf(meteoModel.getTemperature()));
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        //
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        btnSwitchModeShoreOffShore.setText(meteoModel.getMeteoType().toString());
+        textTemp.setText(String.valueOf(meteoModel.getTemperature()));
     }
 }
---
+++ app/src/main/java/com/thomas/bateau/meteo/MeteoModel.java
@@ -0,0 +1,60 @@
+package com.thomas.bateau.meteo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Observable;
+
+public class MeteoModel extends Observable {
+    private MeteoType meteoType=MeteoType.METEO_MARINE;
+    private Map<MeteoType, Float> listTemperatures=new HashMap<>();
+
+    public MeteoModel() {
+        listTemperatures.put(MeteoType.METEO_COTIERE, new Float(15.0));
+        listTemperatures.put(MeteoType.METEO_MARINE, new Float(10.0));
+    }
+
+    public float getTemperature() {
+        return listTemperatures.get(meteoType);
+    }
+
+    public void setTemperature(MeteoType meteoType, float temperature) {
+        if(!listTemperatures.containsKey(meteoType)) {
+            return;
+        }
+        listTemperatures.put(meteoType, temperature);
+        setChanged();
+        notifyObservers();
+    }
+
+    public MeteoType getMeteoType() {
+        return meteoType;
+    }
+
+    public void setMeteoType(MeteoType meteoType) {
+        this.meteoType=meteoType;
+        setChanged();
+        notifyObservers();
+    }
+
+    public enum MeteoType {
+        METEO_MARINE(0, "Météo marine"),
+        METEO_COTIERE(1, "Météo côtière");
+
+        private String label;
+        private int index;
+        MeteoType(int index, String str) {
+            label=str;
+            this.index=index;
+        }
+
+        public String toString() {
+            return label;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public static MeteoType[] listTypes={METEO_MARINE, METEO_COTIERE};
+    }
+}
