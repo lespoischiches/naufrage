eefd3154d883c7fac63d0ada1bd768785abc6406
Arthur-Soens <71391727+Arthur-Soens@users.noreply.github.com>
2021-04-21 17:17:32 +0200

    fhjsgdsgfgdofpg

--- app/src/main/java/com/thomas/bateau/ParamsActivity.java
+++ app/src/main/java/com/thomas/bateau/ParamsActivity.java
@@ -1,8 +1,10 @@
 package com.thomas.bateau;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +17,7 @@ public class ParamsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int ori = this.getResources().getConfiguration().orientation;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_params);
         rbPecheur=findViewById(R.id.params_radio_pecheur);
@@ -23,27 +26,48 @@ public class ParamsActivity extends AppCompatActivity {
         rbKitter=findViewById(R.id.params_radio_kitter);
         rbSkipper=findViewById(R.id.params_radio_skipper);
         TextView t = findViewById(R.id.textview_paramPenche);
-        switch (BateauApplication.typeUtilisateurs) {
-            case PECHEUR:
-                rbPecheur.setChecked(true);
-                t.setText(getString(R.string.TextPecheurParam));
-                break;
-            case KITTER:
-                rbKitter.setChecked(true);
-                t.setText(getString(R.string.TextKitterParam));
-                break;
-            case SKIPPER:
-                rbSkipper.setChecked(true);
-                t.setText(getString(R.string.TextSkipperParam));
-                break;
-            case PLONGEUR:
-                rbPlongeur.setChecked(true);
-                t.setText(getString(R.string.TextPlongeurParam));
-                break;
-            case SCIENTIFIQUE:
-                rbScientifique.setChecked(true);
-                t.setText(getString(R.string.TextScientifiqueParam));
-                break;
+        if(ori == Configuration.ORIENTATION_LANDSCAPE) {
+            switch (BateauApplication.typeUtilisateurs) {
+                case PECHEUR:
+                    rbPecheur.setChecked(true);
+                    t.setText(getString(R.string.TextPecheurParam));
+                    break;
+                case KITTER:
+                    rbKitter.setChecked(true);
+                    t.setText(getString(R.string.TextKitterParam));
+                    break;
+                case SKIPPER:
+                    rbSkipper.setChecked(true);
+                    t.setText(getString(R.string.TextSkipperParam));
+                    break;
+                case PLONGEUR:
+                    rbPlongeur.setChecked(true);
+                    t.setText(getString(R.string.TextPlongeurParam));
+                    break;
+                case SCIENTIFIQUE:
+                    rbScientifique.setChecked(true);
+                    t.setText(getString(R.string.TextScientifiqueParam));
+                    break;
+            }
+        }
+        else{
+            switch (BateauApplication.typeUtilisateurs) {
+                case PECHEUR:
+                    rbPecheur.setChecked(true);
+                    break;
+                case KITTER:
+                    rbKitter.setChecked(true);
+                    break;
+                case SKIPPER:
+                    rbSkipper.setChecked(true);
+                    break;
+                case PLONGEUR:
+                    rbPlongeur.setChecked(true);
+                    break;
+                case SCIENTIFIQUE:
+                    rbScientifique.setChecked(true);
+                    break;
+            }
         }
         findViewById(R.id.params_btn_retour).setOnClickListener(c -> {
             finish();
@@ -51,33 +75,55 @@ public class ParamsActivity extends AppCompatActivity {
     }
 
     public void onChangeRoleClicked(View view) {
+        int ori = this.getResources().getConfiguration().orientation;
         TextView t = findViewById(R.id.textview_paramPenche);
         boolean checked = ((RadioButton) view).isChecked();
         if(!checked) {
             return;
         }
         // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.params_radio_pecheur:
-                BateauApplication.typeUtilisateurs=TypeUtilisateurs.PECHEUR;
-                t.setText(getString(R.string.TextPecheurParam));
-                break;
-            case R.id.params_radio_scientifique:
-                BateauApplication.typeUtilisateurs=TypeUtilisateurs.SCIENTIFIQUE;
-                t.setText(getString(R.string.TextScientifiqueParam));
-                break;
-            case R.id.params_radio_plongeur:
-                BateauApplication.typeUtilisateurs=TypeUtilisateurs.PLONGEUR;
-                t.setText(getString(R.string.TextPlongeurParam));
-                break;
-            case R.id.params_radio_kitter:
-                BateauApplication.typeUtilisateurs=TypeUtilisateurs.KITTER;
-                t.setText(getString(R.string.TextKitterParam));
-                break;
-            case R.id.params_radio_skipper:
-                BateauApplication.typeUtilisateurs=TypeUtilisateurs.SKIPPER;
-                t.setText(getString(R.string.TextSkipperParam));
-                break;
+        if(ori == Configuration.ORIENTATION_LANDSCAPE) {
+            switch (view.getId()) {
+                case R.id.params_radio_pecheur:
+                    BateauApplication.typeUtilisateurs = TypeUtilisateurs.PECHEUR;
+                    t.setText(getString(R.string.TextPecheurParam));
+                    break;
+                case R.id.params_radio_scientifique:
+                    BateauApplication.typeUtilisateurs = TypeUtilisateurs.SCIENTIFIQUE;
+                    t.setText(getString(R.string.TextScientifiqueParam));
+                    break;
+                case R.id.params_radio_plongeur:
+                    BateauApplication.typeUtilisateurs = TypeUtilisateurs.PLONGEUR;
+                    t.setText(getString(R.string.TextPlongeurParam));
+                    break;
+                case R.id.params_radio_kitter:
+                    BateauApplication.typeUtilisateurs = TypeUtilisateurs.KITTER;
+                    t.setText(getString(R.string.TextKitterParam));
+                    break;
+                case R.id.params_radio_skipper:
+                    BateauApplication.typeUtilisateurs = TypeUtilisateurs.SKIPPER;
+                    t.setText(getString(R.string.TextSkipperParam));
+                    break;
+            }
+        }
+        else{
+            switch (BateauApplication.typeUtilisateurs) {
+                case PECHEUR:
+                    rbPecheur.setChecked(true);
+                    break;
+                case KITTER:
+                    rbKitter.setChecked(true);
+                    break;
+                case SKIPPER:
+                    rbSkipper.setChecked(true);
+                    break;
+                case PLONGEUR:
+                    rbPlongeur.setChecked(true);
+                    break;
+                case SCIENTIFIQUE:
+                    rbScientifique.setChecked(true);
+                    break;
+            }
         }
         if(BateauApplication.mainActivityInstance != null) {
             BateauApplication.mainActivityInstance.reloadBtn();
