689a54c771d519cb87532f680183b7c7746cfbfc
JeanChristopheRIERA <jchris101@hotmail.com>
2021-05-01 20:16:32 +0200

    Ajout du bouton twitter

--- app/src/main/java/com/thomas/bateau/alert/NaufrageActivity.java
+++ app/src/main/java/com/thomas/bateau/alert/NaufrageActivity.java
@@ -1,6 +1,7 @@
 package com.thomas.bateau.alert;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +12,11 @@ import com.thomas.bateau.R;
 public class NaufrageActivity extends LocationAccessActivity {
 
 
-    Button sharingNaufrage, buttonBack ;
+    Button sharingNaufrage, buttonBack, buttonSharePosition;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alert_activity_naufrage);
+        sharingNaufrage = findViewById(R.id.sharePosition);
 
         buttonBack = findViewById(R.id.returnButtonNaufrage);
         buttonBack.setOnClickListener(new View.OnClickListener() {
@@ -24,22 +26,31 @@ public class NaufrageActivity extends LocationAccessActivity {
             }
         });
 
-        sharingNaufrage =findViewById(R.id.sharePosition);
         sharingNaufrage.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(Intent.ACTION_SEND);
-                        intent.setType("text/plain");
-                        Double[] positions  = getLocation();
-                        String shareBody = " "+positions[0]+", "+ positions[1];
-                        String shareSub = "Share Position";
-                        intent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
-                        intent.putExtra(Intent.EXTRA_TEXT,shareBody);
-                        startActivity(Intent.createChooser(intent,"Share Using"));
-                    }
-                });
+                @Override
+                public void onClick(View v) {
+                    Double[] positions  = getLocation();
+                    String shareBody = " "+positions[0]+", "+ positions[1];
+                    String url = "https://twitter.com/intent/tweet?text=Alert%20mon%20embarcation%20sombre,%20ma%20position%20est%20la%20suivante%20:%20" + shareBody;
+                    Intent intent = new Intent( Intent.ACTION_VIEW, Uri.parse( url ) );
+                    startActivity(intent);
+                }
+        });
+
+        /* Le code de William */
+//        sharingNaufrage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_SEND);
+//                intent.setType("text/plain");
+//                Double[] positions  = getLocation();
+//                String shareBody = " "+positions[0]+", "+ positions[1];
+//                String shareSub = "Share Position";
+//                intent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
+//                intent.putExtra(Intent.EXTRA_TEXT,shareBody);
+//                startActivity(Intent.createChooser(intent,"Share Using"));
+//            }
+//        });
 
     }
-
-
 }
