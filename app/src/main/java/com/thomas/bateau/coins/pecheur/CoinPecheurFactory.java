4f9750dd9e2aa011601fa016ad4cb06177cb2849
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-05-02 21:53:31 +0200

    Fix Arthur's misuse of pattern factory

--- app/src/main/java/com/thomas/bateau/coins/pecheur/CoinPecheurFactory.java
+++ app/src/main/java/com/thomas/bateau/coins/pecheur/CoinPecheurFactory.java
@@ -1,5 +1,6 @@
 package com.thomas.bateau.coins.pecheur;
 
+import com.thomas.bateau.R;
 import com.thomas.bateau.coins.CoinsFactory;
 import com.thomas.bateau.coins.ResultCoinFragment;
 import com.thomas.bateau.coins.SearchCoinFragment;
@@ -14,5 +15,10 @@ public class CoinPecheurFactory extends CoinsFactory {
     @Override
     public SearchCoinFragment createFragmentSearch() {
         return new SearchCoinPecheurFragment();
+    }
+
+    @Override
+    public int getTextPresentation() {
+        return R.string.TextCoinPeche;
     }
 }
