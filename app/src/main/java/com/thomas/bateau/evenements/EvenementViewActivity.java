93fda8c521f8bd115a57379e8db78af333413524
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-05-04 22:37:39 +0200

    Evenement: can share

--- app/src/main/java/com/thomas/bateau/evenements/EvenementViewActivity.java
+++ app/src/main/java/com/thomas/bateau/evenements/EvenementViewActivity.java
@@ -17,7 +17,7 @@ public class EvenementViewActivity extends AppCompatActivity {
     private Evenement evenement;
     private TextView textViewTitre, textViewDescription, textViewTexte;
     private ImageView image;
-    private Button btnRetour;
+    private Button btnRetour, btnShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +27,20 @@ public class EvenementViewActivity extends AppCompatActivity {
         textViewDescription=findViewById(R.id.activity_evenement_view_description);
         textViewTexte=findViewById(R.id.activity_evenement_view_texte);
         btnRetour=findViewById(R.id.activity_evenement_view_retour);
+        btnShare=findViewById(R.id.activity_evenement_view_share_btn);
         image=findViewById(R.id.activity_evenement_view_image);
 
         evenement=getIntent().getParcelableExtra(EVENEMENT);
         btnRetour.setOnClickListener(c -> {
             finish();
+        });
+        btnShare.setOnClickListener(c -> {
+            String shareBody = "Je partage avec vous l'événement " + evenement.getTitle() + ":\n" + evenement.getDescription();
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, evenement.getTitle());
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+            startActivity(Intent.createChooser(sharingIntent, "Partager avec"));
         });
 
         textViewTitre.setText(evenement.getTitle());
