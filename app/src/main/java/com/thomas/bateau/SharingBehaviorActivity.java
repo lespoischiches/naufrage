6ac6e8062970c028306a0b1ea911b3406a6b1f3f
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-05-04 23:08:01 +0200

    Add shareTweetLaunchBrowser() in SharingBehaviorActivity

--- app/src/main/java/com/thomas/bateau/SharingBehaviorActivity.java
+++ app/src/main/java/com/thomas/bateau/SharingBehaviorActivity.java
@@ -2,6 +2,12 @@ package com.thomas.bateau;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public interface SharingBehaviorActivity<T extends Activity> {
@@ -11,5 +17,15 @@ public interface SharingBehaviorActivity<T extends Activity> {
         sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareTitle);
         sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
         ((Activity)this).startActivity(Intent.createChooser(sharingIntent, intentTitle));
+    }
+
+    public default void shareTweetLaunchBrowser(String tweet) {
+        try {
+            String url = "https://twitter.com/intent/tweet?text=" + URLEncoder.encode(tweet, "utf-8");
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            ((Activity)this).startActivity(intent);
+        } catch (UnsupportedEncodingException e) {
+            //
+        }
     }
 }
