a4632638d84f6a0b4c6f3e11b66d6bde5924cc43
Arthur-Soens <71391727+Arthur-Soens@users.noreply.github.com>
2021-05-05 00:46:40 +0200

    AlertActivity fix

--- app/src/main/java/com/thomas/bateau/alert/RemainsActivity.java
+++ app/src/main/java/com/thomas/bateau/alert/RemainsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.thomas.bateau.R;
 
@@ -17,13 +18,15 @@ public class RemainsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alert_activity_remains);
 
-        buttonBack = findViewById(R.id.alert_remains_return);
+        buttonBack = findViewById(R.id.alert_fragment_return);
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        ((TextView) findViewById(R.id.alert_fragment_titre)).setText("Signaler un débris");
 
 
     }
